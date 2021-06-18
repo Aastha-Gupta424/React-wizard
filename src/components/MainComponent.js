de8455c47371be4a5f2738ca4992a13a306cb29a
Aastha Gupta <guptaastha424@gmail.com>
2021-06-18 10:28:22 +0530

    first

---
+++ src/components/MainComponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+
+      <div>
+        <p>
+          hi
+        </p>
+      </div>
+    )
+  }
+}
+export default Main;
